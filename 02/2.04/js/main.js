53f650bf4df4c823e103718835ff8cf03cabb781
jdm79 <james.malvern@gmail.com>
2019-12-24 16:22:39 +0000

    adds input box to create different-sized circles

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -4,12 +4,46 @@
 *    2.4 - Adding SVGs with D3
 */
 
-var svg = d3.select("#chart-area").append("svg")
-	.attr("width", 400)
-	.attr("height", 400);
+// var svg = d3.select("#chart-area").append("svg")
+// 	.attr("width", 900)
+// 	.attr("height", 900);
 
-var circle = svg.append("circle")
-	.attr("cx", 100)
-	.attr("cy", 250)
-	.attr("r", 70)
-	.attr("fill", "grey");
+// var circle = svg.append("rect")
+// 	.attr("x", 000)
+// 	.attr("y", 300)
+// 	.attr("height", 170)
+// 	.attr("width", 340)
+//   .attr("fill", "grey");
+
+
+
+let r = 0
+let radius = 0
+let svg 
+let circle
+
+const drawCircle = () => {
+  if (count > 0) {
+    location.reload()
+  }
+
+  circle = svg.append("circle")
+    .attr("cx", 500)
+    .attr("cy", 250)
+    .attr("r", radius)
+    .attr("fill", "blue")
+    count += 1
+}
+
+const myFunc = () => {
+
+  radius = document.getElementById("radius").value
+
+  document.getElementById("diameter").innerHTML = radius * 2
+  r = radius
+  svg = d3.select("#canvas").append("svg")
+  .attr("width", 1000)
+  .attr("height", 1000)
+  drawCircle()
+}
+    
