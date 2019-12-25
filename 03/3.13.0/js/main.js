925ba6d3bd917560e10ae4110f1c98f7ee72065f
jdm79 <james.malvern@gmail.com>
2019-12-25 16:52:45 +0000

    sets canvas size, imports data from json file, parses strings to integers

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -4,3 +4,16 @@
 *    Project 1 - Star Break Coffee
 */
 
+const svg = d3.select("chart-area")
+    .append("svg")
+        .attr("width", 400)
+        .attr("height", 400)
+
+
+d3.json("data/revenues.json").then(function(data){
+    data.forEach(function(d){
+        d.revenue = +d.revenue
+        d.profit = +d.profit
+    })
+    console.log(data)
+})
