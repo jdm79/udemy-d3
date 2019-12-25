86798833ed3dd550499ab192f68ad4310f739194
jdm79 <james.malvern@gmail.com>
2019-12-25 17:02:30 +0000

    sets the months on x axis, revenue on y axis, scales the domain data to range

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -4,10 +4,10 @@
 *    Project 1 - Star Break Coffee
 */
 
-const svg = d3.select("chart-area")
+const svg = d3.select("#chart-area")
     .append("svg")
-        .attr("width", 400)
-        .attr("height", 400)
+        .attr("width", "400")
+        .attr("height", "400")
 
 
 d3.json("data/revenues.json").then(function(data){
@@ -15,5 +15,32 @@ d3.json("data/revenues.json").then(function(data){
         d.revenue = +d.revenue
         d.profit = +d.profit
     })
-    console.log(data)
+    const x = d3.scaleBand()
+        .domain(data.map(function(d){
+            return d.month
+        }))
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.4)
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){
+            return d.revenue
+        })])
+        .range([0, 400])
+
+    const rects = svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("y", 0)
+        .attr("x", function(d){
+            return x(d.month);
+        })
+        .attr("width", x.bandwidth)
+        .attr("height", function(d){
+            return y(d.revenue);
+        })
+        .attr("fill", "grey");
+
 })
