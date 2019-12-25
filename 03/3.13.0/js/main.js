8b31022b66f8c3b0ab01d1823e7c9258760f3347
jdm79 <james.malvern@gmail.com>
2019-12-25 20:11:50 +0000

    adds labels for x and y axes, inverts the bars to sit flush on the botto

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -4,15 +4,34 @@
 *    Project 1 - Star Break Coffee
 */
 
+const paddingInner = 0.3, paddingOuter = 0.4
+
 const margin = { top: 20, right: 10, bottom: 100, left: 100}
-const width = 600 - margin.left - margin.right, 
-      height = 400 -margin.top - margin.bottom
+let width = 600 - margin.left - margin.right, 
+      height = 400 - margin.top - margin.bottom
 
 const g = d3.select("#chart-area").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")") // appends group to canvas, shifts it
+
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Star Break revenue 2019")
+
+g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (height / 2) )
+    .attr("y", - 60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)") // this makes the title run vertically down the y axis
+    .text("Revenue - $")
 
 
 d3.json("data/revenues.json").then(function(data){
@@ -25,27 +44,43 @@ d3.json("data/revenues.json").then(function(data){
             return d.month
         }))
         .range([0, width])
-        .paddingInner(0.3)
-        .paddingOuter(0.4)
+        .paddingInner(paddingInner)
+        .paddingOuter(paddingOuter)
 
     const y = d3.scaleLinear()
         .domain([0, d3.max(data, function(d){
             return d.revenue
         })])
-        .range([0, height])
+        .range([height, 0])
 
+    const xAxisCall = d3.axisBottom(x)
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxisCall)
+        // .selectAll("text")
+        //     .attr("y", "10")
+        //     .attr("x", "-5")
+        //     .attr("text-anchor", "end")
+        //     .attr("transform", "rotate(-40)")
+   
+    const yAxisCall = d3.axisLeft(y)
+        // only use this if you want a measurement after the integer
+        // .tickFormat(function(d){
+        //     return d + "m"
+        // })
+    g.append("g")
+        .attr("class", "y-axis")
+        .call(yAxisCall)
+        
     const rects = g.selectAll("rect")
         .data(data)
-        .enter()
-        .append("rect")
-        .attr("y", 0)
-        .attr("x", function(d){
-            return x(d.month);
-        })
-        .attr("width", x.bandwidth)
-        .attr("height", function(d){
-            return y(d.revenue);
-        })
-        .attr("fill", "grey");
 
+    rects.enter()
+        .append("rect")
+            .attr("y", function(d){return y(d.revenue)})
+            .attr("x", function(d){return x(d.month)})
+            .attr("width", x.bandwidth)
+            .attr("height", function(d){return height - y(d.revenue)})
+            .attr("fill", "grey");
 })
